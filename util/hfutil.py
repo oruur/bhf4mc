d18fffdc28d8803f316aaa842d2ecc494703eb24
Ho-Seok Ee <hoseok.ee@gmail.com>
2023-07-11 16:40:12 +0900

    Combine functions into HFUtil class in hfutil.py

--- util/hfutil.py
+++ util/hfutil.py
@@ -1,205 +1,227 @@
-import cv2, os, json, argparse
+import cv2, os, json
 import numpy as np
 
 
-def importGlyphs(filename, base={}, xshift=0, yshift=0):
-    """
-    Import glyphs in unifont hex or bdf format and convert it to an array of
-    large integers.
-    """
-    shift = 0
-    if xshift: shift += xshift
-    if yshift: shift -= yshift * 16
-
-    if filename and os.path.isfile(filename):
-        with open(filename, 'r') as f:
-            for line in f.readlines():
-                if line[4:5] == ':':
-                    code = int(line[0:4], 16)
-                    bitmap = line.strip()[5:]
-                if line.startswith('ENCODING'):
-                    code = int(line[9:])
-                    bitmap = ''
-                if len(line) in [3, 5]:
-                    bitmap += line.strip()
-                if line.startswith('ENDCHAR') or line[4:5] == ':':
-                    glyph = int(bitmap, 16)
-                    if shift > 0: glyph >>=  shift
-                    if shift < 0: glyph <<= -shift
-                    base.setdefault(code, glyph)
-    return base
+class HFUtil:
+    def __init__(self, filename, shift=0, xshift=0, yshift=0, width=16):
+        self.data = {}
+        self.shift = shift if shift else 0
+        self.width = 16 if width == None else width
+        self.read(filename, shift, xshift, yshift)
+        self.range = [(0x1100, 0x1113), (0x1161, 0x1176), (0x11A8, 0x11C3), (0x3131, 0x3164), (0xAC00, 0xD7A4)]
 
 
-def composeChar(code, base, shift=0):
-    """
-    Compose a glyph of a Hangul char in Unicode plane 0 using the base glyphs.
-    """
-    # Hangul Jamo
-    if   0x1100 <= code < 0x1113: return base[code-0x1100+0x134]
-    elif 0x1161 <= code < 0x1176: return base[code-0x1161+0x1A6]
-    elif 0x11A8 <= code < 0x11C3: return base[code-0x11A8+0x1E5]
+    def read(self, filename, shift=0, xshift=0, yshift=0):
+        """
+        Import glyphs in unifont hex or bdf format and convert it to an array of
+        large integers.
+        """
+        shift = 0
+        if xshift: shift += xshift
+        if yshift: shift -= yshift * 16
 
-    # Hangul Compatibility Jamo
-    elif 0x3131 <= code < 0x3164: return base[code-0x3130+0x100]
-
-    # Hangul Syllables
-    elif 0xAC00 <= code < 0xD7A4:
-        c = code - 0xAC00
-        initial, medial, final = c//28//21, c//28%21, c%28
-
-        # Select initial consonant
-        if medial in [9, 10, 11, 14, 15, 16, 19]:   initial += 19*1 # ㅘ,ㅙ,ㅚ,ㅝ,ㅞ,ㅟ,ㅢ
-        if medial in [8, 12, 13, 17, 18]:           initial += 19*2 # ㅗ,ㅛ,ㅜ,ㅠ,ㅡ
-        if medial in [13, 14, 15, 16, 17] or final: initial += 19*3 # ㅜ,ㅝ,ㅞ,ㅟ,ㅠ
-        glyph = base[0x134+initial]
-
-        # Select and overlay medial vowel
-        if final == 4: medial += 21*1   # ㄴ
-        elif final:    medial += 21*2
-        glyph |= base[0x1A6+medial]
-
-        # Select and overlay final consonant
-        if final:
-            if medial%21 not in [1, 3, 5, 7, 10, 15]: shift = 0     # ㅐ,ㅒ,ㅔ,ㅖ,ㅙ,ㅞ
-            glyph |= base[0x1E4+final] >> shift
-
-    return glyph
+        if filename and os.path.isfile(filename):
+            with open(filename, 'r') as f:
+                for line in f.readlines():
+                    if line[4:5] == ':':
+                        code = int(line[0:4], 16)
+                        bitmap = line.strip()[5:]
+                    if line.startswith('ENCODING'):
+                        code = int(line[9:])
+                        bitmap = ''
+                    if len(line) in [3, 5]:
+                        bitmap += line.strip()
+                    if line.startswith('ENDCHAR') or line[4:5] == ':':
+                        glyph = int(bitmap, 16)
+                        if shift > 0: glyph >>=  shift
+                        if shift < 0: glyph <<= -shift
+                        self.data.setdefault(code, glyph)
 
 
-def convertBase(base, method, shift=0):
-    """
-    Convert base glyphs data to base with different grouping or glyphs of
-    entire Hangul jamos and syllables.
-    """
-    if not method or len(base) > 256:
-        return base
+    def compose_syllable(self, code):
+        """
+        Compose a glyph of a Hangul char in Unicode plane 0 using the base glyphs.
+        """
+        # Hangul Jamo
+        if   self.range[0][0] <= code < self.range[0][1]: return self.data[code-self.range[0][0]+0x134]
+        elif self.range[1][0] <= code < self.range[1][1]: return self.data[code-self.range[1][0]+0x1A6]
+        elif self.range[2][0] <= code < self.range[2][1]: return self.data[code-self.range[2][0]+0x1E5]
 
-    elif method in ['gbc2gbv', 'gbv2gbc']:  # gbc: group by code, gbv: group by variation
-        old = list(base.values())
-        new = old[:]
-        table = [(52,19,6),(54,19,6),(53,19,6),(55,19,6),(57,19,6),(56,19,6),(166,21,3),(168,21,3),(167,21,3)]
-        s = 52
-        for start, size, step in table:
-            if method == 'gbc2gbv': new[s:s+size] = old[start:start+size*step:step]
-            if method == 'gbv2gbc': new[start:start+size*step:step] = old[s:s+size]
-            s += size
-        return {code+0x100: new[code] for code in range(len(new))}
+        # Hangul Compatibility Jamo
+        elif self.range[3][0] <= code < self.range[3][1]: return self.data[code-self.range[3][0]+0x101]
 
-    else:
-        codes = list(range(0x11A8, 0x11C3)) + list(range(0x3131, 0x3164)) + list(range(0xAC00, 0xD7A4))
-        return {code: composeChar(code, base, shift if shift else 0) for code in codes}
+        # Hangul Syllables
+        elif self.range[4][0] <= code < self.range[4][1]:
+            c = code - self.range[4][0]
+            initial, medial, final = c//28//21, c//28%21, c%28
 
+            # Select initial consonant
+            if medial in [9, 10, 11, 14, 15, 16, 19]:   initial += 19*1 # ㅘ,ㅙ,ㅚ,ㅝ,ㅞ,ㅟ,ㅢ
+            if medial in [8, 12, 13, 17, 18]:           initial += 19*2 # ㅗ,ㅛ,ㅜ,ㅠ,ㅡ
+            if medial in [13, 14, 15, 16, 17] or final: initial += 19*3 # ㅜ,ㅝ,ㅞ,ㅟ,ㅠ
+            glyph = self.data[0x134+initial]
 
-def exportHEX(filename, glyphs):
-    """
-    Generate unifont hex file from glyphs data.
-    """
-    glyphs = importGlyphs(filename, glyphs)
+            # Select and overlay medial vowel
+            if final == 4: medial += 21*1 # ㄴ
+            elif final:    medial += 21*2
+            glyph |= self.data[0x1A6+medial]
 
-    with open(filename, 'w') as f:
-        for key, value in sorted(glyphs.items()):
-            if key < 0x100:
-                f.write(f'{key:04X}:{value:032X}\n')
-            else:
-                f.write(f'{key:04X}:{value:064X}\n')
+            # Select and overlay final consonant
+            if final:
+                if medial%21 in [1, 3, 5, 7, 10, 15]: # ㅐ,ㅒ,ㅔ,ㅖ,ㅙ,ㅞ
+                    glyph |= self.data[0x1E4+final] >> self.shift
+                else:
+                    glyph |= self.data[0x1E4+final]
 
-
-def exportBDF(filename, glyphs):
-    """
-    Generate bdf font file from glyphs data.
-    """
-    glyphs = importGlyphs(filename, glyphs)
-
-    with open(filename, 'w') as f:
-        f.write('STARTFONT 2.1\nFONT Font\nSIZE 16 75 75\nFONTBOUNDINGBOX 16 16 0 -2\n')
-        f.write('STARTPROPERTIES 5\nPIXEL_SIZE 16\nPOINT_SIZE 160\nSPACING "C"\n')
-        f.write(f'FONT_ASCENT 14\nFONT_DESCENT 2\nENDPROPERTIES\nCHARS {len(glyphs)}\n')
-
-        for key, value in sorted(glyphs.items()):
-            f.write(f'STARTCHAR U+{key:04X}\nENCODING {key}')
-            if key < 0x100:
-                f.write('\nSWIDTH 500 0\nDWIDTH 8 0\nBBX 8 16 0 -2\nBITMAP\n')
-                f.write('\n'.join([f'{value:032X}'[i:i+2] for i in range(0, 32, 2)]))
-            else:
-                f.write('\nSWIDTH 960 0\nDWIDTH 16 0\nBBX 16 16 0 -2\nBITMAP\n')
-                f.write('\n'.join([f'{value:064X}'[i:i+4] for i in range(0, 64, 4)]))
-            f.write('\nENDCHAR\n')
-
-        f.write('ENDFONT\n')
+        return glyph
 
 
-def makeBitmap(glyph, width=16):
-    """
-    Convert glyph data in large integer to bitmap data in numpy array.
-    """
-    if width and width <= 8:
-        bitmap = np.array([int(b) for b in f'{glyph:0128b}'], dtype=np.uint8).reshape(16, 8)
-    else:
-        bitmap = np.array([int(b) for b in f'{glyph:0256b}'], dtype=np.uint8).reshape(16, 16)
-        if width:
-            bitmap = bitmap[:,:width]
+    def convert(self, method=''):
+        """
+        Convert base glyphs data to base with different grouping or glyphs of
+        entire Hangul jamos and syllables (default: gbv2utf).
+         * gbc2utf: base glyphs sorted in group by code to unicode
+         * gbv2utf: base glyphs sorted in group by variation to unicode
+         * gbc2gbv: base glyphs in group by code to group by variation
+         * gbv2gbc: base glyphs in group by variation to group by code
+        """
+        if method == 'gbc2utf':
+            self.convert('gbc2gbv')
+            self.convert('gbv2utf')
+
+        elif method in ['gbc2gbv', 'gbv2gbc']:
+            old = [self.data[c] for c in range(0x134, 0x1E5)]
+            new = old[:]
+            first  = 0
+            table  = [(i,      19, 6) for i in (0, 2, 1, 3, 5, 4)]
+            table += [(i+19*6, 21, 3) for i in (0, 2, 1)]
+            for start, size, step in table:
+                if method == 'gbc2gbv': new[first:first+size] = old[start:start+size*step:step]
+                if method == 'gbv2gbc': new[start:start+size*step:step] = old[first:first+size]
+                first += size
+            for c in range(19*6 + 21*3):
+                self.data[c+0x134] = new[c]
+
+        elif method:
+            for start, stop in self.range:
+                for code in range(start, stop): self.data[code] = self.compose_syllable(code)
+            for code in range(0x0100, 0x1000):
+                self.data.pop(code, None)
+
+
+    def export_hex(self, filename):
+        """
+        Export glyphs data to unifont hex file.
+        """
+        self.read(filename)
+
+        with open(filename, 'w') as f:
+            for key, value in sorted(self.data.items()):
+                if key < 0x100:
+                    f.write(f'{key:04X}:{value:032X}\n')
+                else:
+                    f.write(f'{key:04X}:{value:064X}\n')
+
+
+    def export_bdf(self, filename):
+        """
+        Export glyphs data to bdf font file.
+        """
+        self.read(filename)
+
+        with open(filename, 'w') as f:
+            f.write('STARTFONT 2.1\nFONT Font\nSIZE 16 75 75\nFONTBOUNDINGBOX 16 16 0 -2\n')
+            f.write('STARTPROPERTIES 5\nPIXEL_SIZE 16\nPOINT_SIZE 160\nSPACING "C"\n')
+            f.write(f'FONT_ASCENT 14\nFONT_DESCENT 2\nENDPROPERTIES\nCHARS {len(self.data)}\n')
+
+            for key, value in sorted(self.data.items()):
+                f.write(f'STARTCHAR U+{key:04X}\nENCODING {key}')
+                if key < 0x100:
+                    f.write('\nSWIDTH 500 0\nDWIDTH 8 0\nBBX 8 16 0 -2\nBITMAP\n')
+                    f.write('\n'.join([f'{value:032X}'[i:i+2] for i in range(0, 32, 2)]))
+                else:
+                    f.write('\nSWIDTH 960 0\nDWIDTH 16 0\nBBX 16 16 0 -2\nBITMAP\n')
+                    f.write('\n'.join([f'{value:064X}'[i:i+4] for i in range(0, 64, 4)]))
+                f.write('\nENDCHAR\n')
+
+            f.write('ENDFONT\n')
+
+
+    def bitmap(self, code):
+        """
+        Return bitmap of a char in numpy array.
+        """
+        glyph = self.data.get(code, 0)
+
+        if code < 0x100:
+            bitmap = np.array([int(b) for b in f'{glyph:0128b}'], dtype=np.uint8).reshape(16, 8)
         else:
-            bitmap = np.pad(bitmap[:,:1+np.nonzero(np.any(bitmap, axis=0))[0][-1]], ((0, 0), (0, 2)))
+            bitmap = np.array([int(b) for b in f'{glyph:0256b}'], dtype=np.uint8).reshape(16, 16)
 
-    return bitmap
+        if 0 < self.width < 16:
+            bitmap = bitmap[:,:(self.width+1//2) if code < 0x100 else self.width]
+        elif self.width <= 0 and code != 32 and code != 256: # variable width
+            bitmap = np.pad(bitmap[:,:1+np.nonzero(np.any(bitmap, axis=0))[0][-1]], ((0, 0), (0, -self.width)))
+
+        return bitmap
 
 
-def exportPNG(filename, glyphs, cols=0, rows=0, width=16, rgba=False, text=None):
-    """
-    Generate an image containing cols*rows chars from glyphs data.
-    """
-    plane = []
-
-    if text:
-        glyphs[0x20] = 0
-        for line in text.strip().split('\n'):
+    def export_png(self, filename, text, linespace=0, rgba=False):
+        """
+        Export glyphs data to png file with text.
+        """
+        plane = []
+        for line in text:
             if (line):
-                line = [makeBitmap(glyphs[ord(c)], 8 if ord(c) < 0x100 else width) for c in line]
+                line = [self.bitmap(ord(char)) for char in line.rstrip()]
                 plane.append(np.concatenate(line, axis=1))
             else:
                 plane.append(np.zeros((16, 1), dtype=np.uint8))
+
         width = max([len(p[0]) for p in plane])
         for i in range(len(plane)):
-            plane[i] = np.pad(plane[i], ((0, 2), (0, width-len(plane[i][0]))))
+            plane[i] = np.pad(plane[i], ((linespace//2, (linespace+1)//2), (0, width-len(plane[i][0]))))
 
-    else:
-        glyphs = list(glyphs.values())
-        for i in range(rows):
-            line = []
-            for j in range(cols):
-                line.append(makeBitmap(glyphs[cols*i+j] if cols*i+j < len(glyphs) else 0))
-            plane.append(np.concatenate(line, axis=1))
-
-    plane = np.concatenate(plane)
-    if rgba: cv2.imwrite(filename, 255 * cv2.merge([plane, plane, plane, plane]))
-    else: cv2.imwrite(filename, 255 * (1-plane))
-
-
-def createJSON(jsonfile, output, glyphs, cols, rows):
-    """
-    Create JSON file for Minecraft font resource pack.
-    """
-    with open(jsonfile, 'w') as f:
-        provider = {}
-
-        if output.endswith('.hex'):
-            provider['type'] = 'unihex'
-            provider['hex_file'] = f'minecraft:font/{output[:-4]}.zip'
-            provider['size_overrides'] = [{
-                        'from': chr(0x1100),
-                        'to': chr(0xFF00),
-                        'left': 0,
-                        'right': 14
-                    }]
-
+        plane = np.concatenate(plane)
+        if rgba:
+            cv2.imwrite(filename, 255 * cv2.merge([plane, plane, plane, plane]))
         else:
-            provider['type'] = 'bitmap'
-            provider['file'] = f'minecraft:font/{output[:-4]}.png'
-            provider['ascent'] = 8
-            provider['chars'] = []
-            for i in range(rows):
-                provider['chars'].append('')
-                for j in range(cols):
-                    provider['chars'][-1] += chr(glyphs[cols*i+j] if cols*i+j < len(glyphs) else 0)
+            cv2.imwrite(filename, 255 * (1-plane))
 
-        json.dump({'providers': [provider]}, f, indent='\t', separators=(',', ': '))
+
+    def export(self, filename, text=[], linespace=0, rgba=False):
+        """
+        Export glyphs data to bdf, hex, or png file.
+        """
+        if filename.endswith('.bdf'): self.export_bdf(filename)
+        if filename.endswith('.hex'): self.export_hex(filename)
+        if filename.endswith('.png'): self.export_png(filename, text, linespace, rgba)
+
+
+    def json(self, jsonfile, datafile, text):
+        """
+        Create JSON file for Minecraft font resource pack.
+        """
+        with open(jsonfile, 'w') as f:
+            provider = {}
+
+            if datafile.endswith('.hex'):
+                provider['type'] = 'unihex'
+                provider['hex_file'] = f'minecraft:font/{datafile[:-4]}.zip'
+                provider['size_overrides'] = [{
+                            'from': chr(0x1100),
+                            'to': chr(0xFF00),
+                            'left': 0,
+                            'right': 14
+                        }]
+            else:
+                provider['type'] = 'bitmap'
+                provider['file'] = f'minecraft:font/{datafile[:-4]}.png'
+                provider['ascent'] = 8
+                provider['chars'] = []
+
+                for line in text:
+                    if (line):
+                        provider['chars'].append(line.strip())
+
+            json.dump({'providers': [provider]}, f, indent='\t', separators=(',', ': '))
